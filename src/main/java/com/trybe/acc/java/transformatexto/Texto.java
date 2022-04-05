2a5aa37f0bdb5a0875296255974fd595b4f8a726
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-04-04 21:21:15 -0300

    finish

--- src/main/java/com/trybe/acc/java/transformatexto/Texto.java
+++ src/main/java/com/trybe/acc/java/transformatexto/Texto.java
@@ -1,5 +1,16 @@
 package com.trybe.acc.java.transformatexto;
 
 public class Texto {
+  String frase = "Lorem+ipsum+dolort sit ammet";
 
+  public String transformaTexto(String frase) {
+    frase = frase.replace('+', ' ');
+    if (frase.indexOf('a') % 2 == 0) {
+      frase = frase.toUpperCase();
+    } else {
+      String[] fraseArray = frase.split(" ", 2);
+      return fraseArray[0];
+    }
+    return frase;
+  }
 }
